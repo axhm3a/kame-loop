a86bd85063f948dc210be97bb78f0f005464513b
Daniel Basten <axhm3a@gmail.com>
2018-12-02 18:34:59 +0100

    Initialer Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+functional gameloop for kotlin
+==============================
+
+
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,33 @@
+/**
+ *
+ *   functional game loop
+ *   in Kotlin
+ *
+ */
+fun createGameLoop(callbacks: List<(Long) -> Unit>): (Long) -> Long {
+
+    val gameloop: (Long) -> Long = { delta:Long ->
+
+        val start = System.currentTimeMillis()
+
+        callbacks.forEach { it(delta) }
+
+        val end = System.currentTimeMillis()
+
+        end - start
+    }
+
+    return gameloop
+}
+
+fun main(args: Array<String>) {
+
+    val callbacks = listOf<(Long) -> Unit>(
+        {l: Long -> println(l)},
+        {l: Long -> Thread.sleep(500)}
+    )
+
+    val l = createGameLoop(callbacks);
+
+    generateSequence(0L){ it + 1 }.fold(0L){ n, m -> l(n) }
+}
